cb421fe48f6a8e32d6f6c42a1f815634f8ea5e94
darkgod1kandarp <009kandarp@gmail.com>
2021-06-08 09:37:56 +0530

    saved draft showing

--- Routes/blogDraftSaving.routes.js
+++ Routes/blogDraftSaving.routes.js
@@ -1,12 +1,11 @@
-const blogDraftSaving = ({req,res})=>{
+const blogDraftSaving = ({req,res,db})=>{
 
     const { userid } = req.user;
     const { blog,heading } = req.query;
-
     let sql ;
     sql = `select ur_cirkle.new_function('${blog}', '${userid}','${heading}');`
 
-    const[gettingresult]  =  await query(sql);
+    const[gettingresult]  =  await db.query(sql);
 
     const result = gettingresult.result;
 
